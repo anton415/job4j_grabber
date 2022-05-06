16a92e568fea6ce9f9ea296cc74035a884c39c03
Anton Serdyuchenko <anton415@gmail.com>
2022-05-06 10:59:24 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #297550]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -13,7 +13,7 @@ public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     public static void main(String[] args) throws IOException {
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             String pageLink = String.format("%s/vacancies/java_developer?page=%s", SOURCE_LINK, i);
             Connection connection = Jsoup.connect(pageLink);
             Document document = connection.get();
